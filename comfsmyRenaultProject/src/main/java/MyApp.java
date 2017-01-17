5909643c51736fcee90d649bf841a74ec84ca8e4
SEBAOUI <sebaoui@ubuntu.ubuntu-domain>
2017-01-17 17:36:46 +0100

    first commit

---
+++ comfsmyRenaultProject/src/main/java/MyApp.java
@@ -0,0 +1,27 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class MyApp {
+
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args){
+
+
+        SpringApplication.run(MyApp.class,args);
+
+
+
+
+    }
+}
